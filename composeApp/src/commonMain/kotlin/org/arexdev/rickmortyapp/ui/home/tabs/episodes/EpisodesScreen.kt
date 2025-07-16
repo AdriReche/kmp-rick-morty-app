565797d05f0d16fa62b30e834c34b8dfaf68bea1
ew3w6bu <adrian.reche@volkswagen-groupservices.com>
2025-07-16 16:30:09 +0200

    Add video player component and integrate with episode selection

--- composeApp/src/commonMain/kotlin/org/arexdev/rickmortyapp/ui/home/tabs/episodes/EpisodesScreen.kt
+++ composeApp/src/commonMain/kotlin/org/arexdev/rickmortyapp/ui/home/tabs/episodes/EpisodesScreen.kt
@@ -1,18 +1,28 @@
 package org.arexdev.rickmortyapp.ui.home.tabs.episodes
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import app.cash.paging.compose.collectAsLazyPagingItems
@@ -21,11 +31,13 @@ import org.arexdev.rickmortyapp.domain.model.SeasonEpisode
 import org.arexdev.rickmortyapp.ui.core.components.PagingLoadingState
 import org.arexdev.rickmortyapp.ui.core.components.PagingType
 import org.arexdev.rickmortyapp.ui.core.components.PagingWrapper
+import org.arexdev.rickmortyapp.ui.core.components.VideoPlayer
 import org.jetbrains.compose.resources.DrawableResource
 import org.jetbrains.compose.resources.painterResource
 import org.koin.compose.viewmodel.koinViewModel
 import org.koin.core.annotation.KoinExperimentalAPI
 import rickmortyapp.composeapp.generated.resources.Res
+import rickmortyapp.composeapp.generated.resources.portal
 import rickmortyapp.composeapp.generated.resources.season1
 import rickmortyapp.composeapp.generated.resources.season2
 import rickmortyapp.composeapp.generated.resources.season3
@@ -42,23 +54,47 @@ fun EpisodesScreen() {
     val state by episodesViewModel.state.collectAsState()
     val episodes = state.episodes.collectAsLazyPagingItems()
 
-    Box(Modifier.fillMaxSize())
+    Column(Modifier.fillMaxSize())
     {
         PagingWrapper(
             pagingType = PagingType.ROW,
             pagingItems = episodes,
             initialView = { PagingLoadingState() },
             itemView = {
-                EpisodeItemList(it)
+                EpisodeItemList(it, { url -> episodesViewModel.onPlaySelected(url) })
             }
         )
+        EpisodePlayer(playVideo = state.playVideo, onCloseVideo = { episodesViewModel.onCloseVideo() })
     }
 }
 
+@Composable
+fun EpisodePlayer(playVideo: String, onCloseVideo: () -> Unit) {
+    AnimatedVisibility(playVideo.isNotBlank()) {
+        ElevatedCard(
+            modifier = Modifier.fillMaxWidth().height(250.dp).padding(16.dp)
+                .border(3.dp, Color.Green, CardDefaults.elevatedShape)
+        ) {
+            Box(modifier = Modifier.background(Color.Black)) {
+                Box(contentAlignment = Alignment.Center, modifier = Modifier.padding(16.dp)) {
+                    VideoPlayer(modifier = Modifier.fillMaxWidth().height(200.dp), url = playVideo)
+                }
+                Row {
+                    Spacer(modifier = Modifier.weight(1f))
+                    Image(
+                        painter = painterResource(Res.drawable.portal),
+                        contentDescription = "",
+                        modifier = Modifier.padding(8.dp).size(40.dp).clickable { onCloseVideo() }
+                    )
+                }
+            }
+        }
+    }
+}
 
 @Composable
-fun EpisodeItemList(episodeModel: EpisodeModel) {
-    Column(Modifier.width(120.dp).padding(8.dp).clickable {}) {
+fun EpisodeItemList(episodeModel: EpisodeModel, onEpisodeSelected: (String) -> Unit) {
+    Column(Modifier.width(120.dp).padding(8.dp).clickable { onEpisodeSelected(episodeModel.videoUrl) }) {
         Image(
             modifier = Modifier.height(200.dp).fillMaxWidth(),
             contentDescription = null,
